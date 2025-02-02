0935d370cc63ba0ed272b13ebe629b22ce0f6d90
laurenz <laurenz.dilba@hotmail.de>
2025-02-02 21:15:46 +0100

    tweaked style

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,9 +1,10 @@
 from dash import Dash
 import layout
 import callbacks
+import dash_bootstrap_components as dbc
 
 # Dash App initialisieren
-app = Dash()
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Layout importieren
 app.layout = layout.create_layout()
--- dash_app/layout.py
+++ dash_app/layout.py
@@ -1,4 +1,5 @@
 from dash import html, dcc
+import dash_bootstrap_components as dbc
 import data_processing
 
 
@@ -6,20 +7,32 @@ def create_layout():
     """Erstellt das Dash-Layout."""
     fiscal_start, fiscal_end = data_processing.get_fiscal_year_range()
 
-    return html.Div([
-        html.H1(children='Dash App', style={'textAlign': 'center'}),
+    return dbc.Container([
+        # Überschrift links
+        dbc.Row([
+            dbc.Col(html.H2("Faktura Statistics"), width=4),
+            dbc.Col(
+                html.Div(
+                    dcc.DatePickerRange(
+                        id='date-picker-range',
+                        start_date=fiscal_start,
+                        end_date=fiscal_end,
+                        display_format='YYYY-MM-DD',
+                    ),
+                    className="d-flex justify-content-center"
+                ),
+                width=4
+            ),
+            dbc.Col(width=4),
+        ], className="mb-3 mt-3 mx-3", justify='between'),
 
-        # Datumsauswahl (Start- und Enddatum)
-        dcc.DatePickerRange(
-            id='date-picker-range',
-            start_date=fiscal_start,
-            end_date=fiscal_end,
-            display_format='YYYY-MM-DD'
-        ),
+        # Graphen unter dem DatePicker
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='gauge-content'), width=12)
+        ]),
 
-        # Gauge Diagramm
-        dcc.Graph(id='gauge-content'),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='graph-content'), width=12)
+        ])
+    ], fluid=True)
 
-        # Balkendiagramm
-        dcc.Graph(id='graph-content')
-    ])
